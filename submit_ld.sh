f7b24ce31587aa580293abf83a5b328d1805a18d
CAWarmerdam <robert_warmerdam@hotmail.com>
2023-09-20 19:51:45 +0300

    updates

--- submit_ld.sh
+++ submit_ld.sh
@@ -18,32 +18,34 @@ set -f
 # NB! Following will extract all the genes and SNPs! In case of full mapping,
 # it is advisable to use gene filter or P-value filter to get manageable subset!
 
-nextflow_path=/gpfs/space/GI/eQTLGen/tools/
+nextflow_path=/gpfs/space/GI/eQTLGen/freeze2/tools
 
-empirical=/gpfs/space/GI/eQTLGen/freeze1/eqtl_mapping/output/empirical_4GenPC20ExpPC_2023-05-02_PerGene/
+empirical=/gpfs/space/GI/eQTLGen/freeze2/eqtl_mapping/output/empirical_4GenPC20ExpPC_2023-05-10_perGene/
 permuted=/gpfs/space/GI/eQTLGen/freeze1/eqtl_mapping/output/permuted_4GenPC20ExpPC_2023-04-18_PerGene/
 
 reference_data='/gpfs/space/GI/eQTLGen/make_reference_files/30x_reference/hg38/phasing_reference/phasing/chr*.bcf'
 
-genome_reference="hg38.genome"
-variant_reference="/gpfs/space/GI/eQTLGen/freeze1/eqtl_mapping/MetaAnalysis/bin/hase/data/1000G-30x.ref.gz"
-gene_reference="gencode.v43.basic.annotation.gff3.gz"
+genome_reference="/gpfs/space/GI/eQTLGen/freeze1/Interpretation/ld/ExtractMetaAnalysisResults/hg38.genome"
+variant_reference="/gpfs/space/GI/eQTLGen/make_reference_files/30x_reference/hg38/hase_reference/1000G-30x.ref.gz"
+gene_reference="/gpfs/space/GI/eQTLGen/freeze1/Interpretation/ld/ExtractMetaAnalysisResults/gencode.v43.basic.annotation.gff3.gz"
 
-bed="../data/vuckovic_flanked1mb_na_significant_loci_5mlog9_hg38.bed"
+bed="/gpfs/space/GI/eQTLGen/freeze1/Interpretation/ld/data/vuckovic_flanked1mb_na_significant_loci_5mlog9_hg38.bed"
 
-inclusion_step_output="/gpfs/space/GI/eQTLGen/freeze1/InclusionLists/output"
+inclusion_step_output="/gpfs/space/GI/eQTLGen/freeze2/InclusionLists/output"
+maf_table="/gpfs/space/GI/eQTLGen/freeze2/PreMetaQc/output/MafInformation.txt.gz"
 
-output_folder="/gpfs/space/GI/eQTLGen/freeze1/Interpretation/ld/output"
+output_folder="/gpfs/space/GI/eQTLGen/freeze2/Interpretation/ld/output2"
 
-NXF_VER=23.02.1 ${nextflow_path}/nextflow run ExtractMainResults.nf \
+NXF_VER=23.04.1 ${nextflow_path}/nextflow run ExtractMainResults.nf \
 --empirical ${empirical}/eqtls \
 --permuted ${permuted}/eqtls \
 --reference_data ${reference_data} \
---genes ${permuted}/phenotypes_unique.txt \
+--genes ${empirical}/phenotypes_unique.txt \
 --genome_reference ${genome_reference} \
 --variant_reference ${variant_reference} \
 --gene_reference ${gene_reference} \
 --background_bed ${bed} \
+--maf_table ${maf_table} \
 --output ${output_folder} \
 --inclusion_step_output ${inclusion_step_output} \
 -resume \
